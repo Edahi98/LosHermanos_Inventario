9daab2c813175f85deb59858a973dcf71bbb4910
Edahi98 <garciaedahi3822@gmail.com>
2025-11-27 10:57:48 -0600

    retactor(bd): Edit connection

--- los-hermanos-microservice/core/database/config.ts
+++ los-hermanos-microservice/core/database/config.ts
@@ -2,26 +2,10 @@
 import { Dialect } from 'sequelize';
 import process from 'node:process';
 
-export const development = {
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'loshermanos_dev',
-  host: process.env.DB_HOST || 'localhost',
-  dialect: (process.env.DB_DIALECT || 'postgres') as Dialect,
-};
-
-export const test = {
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'loshermanos_test',
-  host: process.env.DB_HOST || 'localhost',
-  dialect: (process.env.DB_DIALECT || 'postgres') as Dialect,
-};
-
-export const production = {
+export const config = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
-  dialect: (process.env.DB_DIALECT || 'postgres') as Dialect,
+  dialect: 'postgres' as Dialect,
 };
